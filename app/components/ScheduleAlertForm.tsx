525e26df6cd47e39c8d96e5ae07a50763e028958
Angelo Maiele <angelomaiele@gamil.com>
2025-03-20 17:07:54 +0100

    more

--- app/components/ScheduleAlertForm.tsx
+++ app/components/ScheduleAlertForm.tsx
@@ -6,6 +6,7 @@ export default function ScheduleAlertForm({ userId }: { userId: number }) {
     phone_number: "",
     message: "",
     scheduled_date: "",
+    email: "",
   });
 
   const handleChange = (e) => {
@@ -40,6 +41,7 @@ export default function ScheduleAlertForm({ userId }: { userId: number }) {
             phone_number: alertData.phone_number,
             message: alertData.message,
             scheduled_date: alertData.scheduled_date,
+            email: alertData.email,
           }),
         }
       );
@@ -56,6 +58,7 @@ export default function ScheduleAlertForm({ userId }: { userId: number }) {
         phone_number: "",
         message: "",
         scheduled_date: "",
+        email: "",
       });
     } catch (error) {
       console.error(error);
@@ -66,16 +69,31 @@ export default function ScheduleAlertForm({ userId }: { userId: number }) {
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-4">
-        <label className="block text-slate-700 mb-2">Phone Number</label>
-        <input
-          type="text"
-          name="phone_number"
-          value={alertData.phone_number}
-          onChange={handleChange}
-          className="w-full px-4 py-2 border rounded-lg"
-          placeholder="+34600111222"
-          required
-        />
+        {/* <div className="">
+          <label className="block text-slate-700 mb-2">Phone Number</label>
+          <input
+            type="text"
+            name="phone_number"
+            value={alertData.phone_number}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border rounded-lg"
+            placeholder="+34600111222"
+            required
+          />
+        </div> */}
+
+        <div className="">
+          <label className="block text-slate-700 mb-2">Email</label>
+          <input
+            type="text"
+            name="email"
+            value={alertData.email}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border rounded-lg"
+            placeholder="Escribe el mensaje aquí..."
+            required
+          />
+        </div>
       </div>
 
       <div className="mb-4">
