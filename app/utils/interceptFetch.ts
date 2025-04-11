93afc72fb2432657a3a1797215317dc78954e1cc
Angelo Maiele <angelomaiele@gamil.com>
2025-04-11 20:04:00 +0200

    mer

--- app/utils/interceptFetch.ts
+++ app/utils/interceptFetch.ts
@@ -4,31 +4,26 @@ if (typeof window !== "undefined") {
   const originalFetch = window.fetch;
 
   window.fetch = async (url, options = {}) => {
-    const isUrlString = typeof url === "string";
-    const urlString = isUrlString ? url : url?.url || "";
-    const isPublicUrl = urlString.includes("/api/public/");
-    console.log("isPublicUrl", isPublicUrl)
     const storedUser = localStorage.getItem("user");
     const token = storedUser ? JSON.parse(storedUser).access_token : null;
 
     // 🔹 Solo agregar el header de autenticación si hay un token
     const headers = {
       ...options?.headers, // Mantener los headers originales
-      ...(token && !isPublicUrl ? { Authorization: `Bearer ${token}` } : {}),
+      ...(token ? { Authorization: `Bearer ${token}` } : {}), 
     };
 
     try {
       const response = await originalFetch(url, { ...options, headers });
 
       // 🔴 Si el token expiró (401), cerramos sesión y redirigimos
-      if (!isPublicUrl && response.status === 401) {
+      if (response.status === 401) {
         console.log("🔴 TOKEN EXPIRADO - CERRANDO SESIÓN");
         localStorage.removeItem("user");
         showErrorToast("Sesión expirada. Inicia sesión de nuevo.");
 
         if (window.location.pathname !== "/") {
-          console.log("test")
-          // window.location.replace("/"); // Redirige y reemplaza en el historial
+          window.location.replace("/"); // Redirige y reemplaza en el historial
         } else {
           window.location.reload(); // Si ya estamos en "/", recargamos la página
         }
