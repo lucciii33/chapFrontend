fa58ed77fca3eabfbd24f8bc41d2c0185fec5dc2
Angelo Maiele <angelomaiele@gamil.com>
2025-08-07 20:47:38 +0200

    more

--- i18n.ts
+++ i18n.ts
@@ -4,7 +4,7 @@ import { initReactI18next } from "react-i18next";
 
 import en from "./app/locales/en/common.json";
 import es from "./app/locales/es/common.json";
-
+console.log("HOLAAA LLAMANDOOOOOO")
 i18n
   .use(initReactI18next)
   .init({
@@ -19,5 +19,24 @@ i18n
       escapeValue: false,
     },
   });
+
+  if (typeof window !== "undefined") {
+  const savedLang = localStorage.getItem("lang");
+
+  if (!savedLang) {
+    fetch(`${import.meta.env.VITE_REACT_APP_URL}/users/test-country`)
+      .then((res) => res.json())
+      .then((data) => {
+        const { lang } = data;
+        console.log("lang", lang);
+        i18n.changeLanguage(lang);
+        localStorage.setItem("lang", lang);
+      })
+      .catch((err) => console.error("Error test-country:", err));
+  } else {
+    i18n.changeLanguage(savedLang);
+  }
+}
+
 
 export default i18n;
